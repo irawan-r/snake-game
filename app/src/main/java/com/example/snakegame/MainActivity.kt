3ced88f45f9d4e9cb3472ac5c143740123776c09
roy <irawanr567@gmail.com>
2024-04-11 16:38:25 +0700

    initial commit

---
+++ app/src/main/java/com/example/snakegame/MainActivity.kt
@@ -0,0 +1,208 @@
+package com.example.snakegame
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
+import com.example.snakegame.ui.theme.SnakeGameTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import kotlin.random.Random
+import kotlin.time.Duration.Companion.milliseconds
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val game = Game(lifecycleScope)
+        setContent {
+            SnakeGameTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Snake(game = game)
+                }
+            }
+        }
+    }
+}
+
+data class State(val food: Pair<Int, Int>, val snake: List<Pair<Int, Int>>)
+
+class Game(private val scope: CoroutineScope) {
+
+    private val mutex = Mutex()
+    private val mutableState =
+        MutableStateFlow(State(food = Pair(5, 5), snake = listOf(Pair(7, 7))))
+
+    val state = mutableState.asStateFlow()
+
+    var move = Pair(1, 0)
+        set(value) {
+            scope.launch {
+                mutex.withLock {
+                    field = value
+                }
+            }
+        }
+
+    init {
+        scope.launch {
+            var snakeLength = 4
+
+            while (true) {
+                delay(150)
+                mutableState.update {
+                    val newPosition = it.snake.first().let { pos ->
+                        mutex.withLock {
+                            Pair(
+                                (pos.first + move.first + BOARD_SIZE) % BOARD_SIZE,
+                                (pos.second + move.second + BOARD_SIZE) % BOARD_SIZE
+                            )
+                        }
+                    }
+
+                    if (newPosition == it.food) {
+                        snakeLength++
+                    }
+
+                    if (it.snake.contains(newPosition)) {
+                        snakeLength = 4
+                    }
+
+                    it.copy(
+                        food = if (newPosition == it.food) Pair(
+                            Random.nextInt(BOARD_SIZE),
+                            Random.nextInt(BOARD_SIZE)
+                        ) else it.food,
+                        snake = listOf(newPosition) + it.snake.take(snakeLength - 1)
+                    )
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val BOARD_SIZE = 16
+    }
+}
+
+@Composable
+fun Snake(game: Game) {
+    val state = game.state.collectAsState(null)
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        state.value?.let {
+            Board(state = it)
+        }
+        Buttons {
+            game.move = it
+        }
+    }
+}
+
+@Composable
+fun Buttons(onDirectionChange: (Pair<Int, Int>) -> Unit) {
+    val buttonSize = Modifier.size(64.dp)
+    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.padding(24.dp)) {
+        Button(onClick = { onDirectionChange(Pair(0, -1)) }, modifier = buttonSize) {
+            Icon(imageVector = Icons.Default.KeyboardArrowUp, contentDescription = null)
+        }
+        Row {
+            Button(onClick = { onDirectionChange(Pair(-1, 0)) }, modifier = buttonSize) {
+                Icon(imageVector = Icons.Default.KeyboardArrowLeft, contentDescription = null)
+            }
+            Spacer(modifier = buttonSize)
+            Button(onClick = { onDirectionChange(Pair(1, 0)) }, modifier = buttonSize) {
+                Icon(imageVector = Icons.Default.KeyboardArrowRight, contentDescription = null)
+            }
+        }
+        Button(onClick = { onDirectionChange(Pair(0, 1)) }, modifier = buttonSize) {
+            Icon(imageVector = Icons.Default.KeyboardArrowDown, contentDescription = null)
+        }
+    }
+}
+
+@Composable
+fun Board(state: State) {
+    BoxWithConstraints(modifier = Modifier.padding(16.dp)) {
+        val tileSize = maxWidth / Game.BOARD_SIZE
+
+        Box(
+            modifier = Modifier
+                .size(maxWidth)
+                .border(2.dp, Color.DarkGray, shape = RoundedCornerShape(16.dp))
+        )
+        Box(
+            modifier = Modifier
+                .offset(
+                    x = tileSize * state.food.first,
+                    y = tileSize * state.food.second
+                )
+                .size(tileSize)
+                .background(
+                    Color.DarkGray, CircleShape
+                )
+        )
+
+        state.snake.forEach {
+            Box(
+                modifier = Modifier
+                    .offset(x = tileSize * it.first, y = tileSize * it.second)
+                    .size(tileSize)
+                    .background(
+                        Color.DarkGray, Shapes().small
+                    )
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SnakeGameTheme {
+
+    }
+}
